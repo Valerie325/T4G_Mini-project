122272aefd36466ad0739087944286e73874cacd
Valerie325 <morevimgh14@gmail.com>
2025-01-06 14:16:00 -1200

    hover

--- src/flavours.js
+++ src/flavours.js
@@ -11,3 +11,27 @@ document.addEventListener("DOMContentLoaded", function () {
     //Display a personalized welcome message
     alert(`Hi ${friend.trim()}, Welcome to Delicious Cupcakes. Feel free to choose from our wide range of cupcake flavours 😍`);
     });
+
+
+
+       // JavaScript for Modal
+       const viewDetailsIcons = document.querySelectorAll('.fa-eye');
+       const modal = document.getElementById('details-modal');
+       const closeModal = document.getElementById('close-modal');
+   
+       viewDetailsIcons.forEach(icon => {
+         icon.addEventListener('click', function () {
+           modal.style.display = 'flex';
+         });
+       });
+   
+       closeModal.addEventListener('click', function () {
+         modal.style.display = 'none';
+       });
+   
+       window.addEventListener('click', function (event) {
+         if (event.target === modal) {
+           modal.style.display = 'none';
+         }
+       });
+
